2ac08291896ef67da51b08e1d6823115d54a441e
Devesh Pal <newdev0@outlook.com>
2022-09-24 10:49:52 +0530

    Update

--- pages/_app.p.js
+++ pages/_app.p.js
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <><Head>
+    <meta name="propeller" content="8491c7374a9512bb83958fdf4ecd0a00">
+    </meta></Head><Component {...pageProps} /></>
 }
 
 export default MyApp
