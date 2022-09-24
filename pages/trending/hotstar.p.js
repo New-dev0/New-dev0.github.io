6aa443a3779a6ddcc1a491a019decd47c1998d16
Auto <Newdev0Bot@outlook.com>
2022-09-24 09:38:09 +0000

    Update Content

---
+++ pages/trending/hotstar.p.js
@@ -0,0 +1,58 @@
+import {Template} from "../_template";
+import styles from "../../styles/Projects.module.css";
+import Head from "next/head";
+import { useMediaQuery } from './../useMediaQuery';
+import Image from "next/image";
+
+export default function Page() {
+    const md = useMediaQuery(960);
+    return <><Head>
+        <title>Hotstar</title></Head><Template>
+            <h1 style={{fontSize: "30"}}>Hotstar</h1>
+            <Image width={800} height={500} src="https://indtoday.com/wp-content/uploads/2022/09/airtel.jpg" alt="Hotstar"/>
+            <h3>Recent News</h3>
+            <a href='https://indtoday.com/airtel-giving-free-disney-plus-hotstar-subscription-with-these-plans-check-here/'>
+        <div className={styles.card} style={{ maxWidth: "90%", width: md ? "auto": "90vh"}}>
+            <h3>Airtel giving free Disney Plus Hotstar subscription with these plans ...</h3>
+            <div style={{ display: "flex" }}>
+            <Image src="https://t1.gstatic.com/images?q=tbn:ANd9GcSUHA5l37UfuQH96K7_GuBlBxPGFOnu-w8oB-YhN2JDeTdnBvAMp7TPcjKwmRiPMlWVUOT2IdKesg" alt="Airtel giving free Disney Plus Hotstar subscription with these plans ..." height={150} width={150} style={{marginLeft: "15px"}}/>
+            <p style={{marginLeft: "15px"}}>The telco offers recharge plans for both prepaid and postpaid users with a free Hotstar subscription along with calling and data benefits. Airtel has introduced&nbsp;...</p></div>
+        </div>
+    </a><a href='https://zeenews.india.com/companies/free-disneyhotstar-subscription-airtel-new-recharge-plans-offering-the-platform-subscription-at-low-cost-2513761.html'>
+        <div className={styles.card} style={{ maxWidth: "90%", width: md ? "auto": "90vh"}}>
+            <h3>Free Disney+Hotstar Subscription: Airtel new recharge plans ...</h3>
+            <div style={{ display: "flex" }}>
+            <Image src="https://t2.gstatic.com/images?q=tbn:ANd9GcShsOVwFNzkg9kT0WDlEgPOjaR6CtoROshqRHNdENrwKskWjXYBDczHFAZ7LoL-Aj_jZdAJCaX7" alt="Free Disney+Hotstar Subscription: Airtel new recharge plans ..." height={150} width={150} style={{marginLeft: "15px"}}/>
+            <p style={{marginLeft: "15px"}}>Airtel has introduced a slew of recharge plans offering free Disney+Hotstar Subscription. Users can avail the subscription of OTT platform with all features&nbsp;...</p></div>
+        </div>
+    </a><a href='https://www.dnaindia.com/india/report-hotstar-subscription-for-3-months-8-gb-data-30-day-validity-but-low-price-is-the-most-stunning-feature-of-t-2987635'>
+        <div className={styles.card} style={{ maxWidth: "90%", width: md ? "auto": "90vh"}}>
+            <h3>Hotstar subscription for 3 months, 8 GB data, 30-day validity, but low ...</h3>
+            <div style={{ display: "flex" }}>
+            <Image src="https://t3.gstatic.com/images?q=tbn:ANd9GcSLQOXEHbixf9Z_25VxwD-yDD-0JJegnOp7Unbse1yVGknO9jDRDm-wwSbKLACIvPpJkWtF7fEC" alt="Hotstar subscription for 3 months, 8 GB data, 30-day validity, but low ..." height={150} width={150} style={{marginLeft: "15px"}}/>
+            <p style={{marginLeft: "15px"}}>Vodaphone-Idea is taking the fight to RelianceJio. The company has been launching super value-for-money prepaid plans that are loaded but easy in the pocket&nbsp;...</p></div>
+        </div>
+    </a><a href='https://telugu.mykhel.com/cricket/blockbuster-trp-rating-for-india-vs-australia-2nd-t20-match-on-hotstar-with-1-crore-043995.html'>
+        <div className={styles.card} style={{ maxWidth: "90%", width: md ? "auto": "90vh"}}>
+            <h3>India vs Australia: బ్లాక్‌బస్టర్ టీఆర్‌పీ రేటింగ్స్.. హాట్‌స్టార్ ఆల్‌టైమ్ ...</h3>
+            <div style={{ display: "flex" }}>
+            <Image src="https://t0.gstatic.com/images?q=tbn:ANd9GcQ9uM-ymmXsv9f8d3NVrNKNCFDT4Ii9_aT1iv18unEBempnrzXpFB95_KsHVxKxMUn7OulmBS3b" alt="India vs Australia: బ్లాక్‌బస్టర్ టీఆర్‌పీ రేటింగ్స్.. హాట్‌స్టార్ ఆల్‌టైమ్ ..." height={150} width={150} style={{marginLeft: "15px"}}/>
+            <p style={{marginLeft: "15px"}}>India vs Australia: బ్లాక్‌బస్టర్ టీఆర్‌పీ రేటింగ్స్.. హాట్‌స్టార్ ఆల్‌టైమ్ రికార్డ్! ఎంతంటే..? &middot; By&nbsp;...</p></div>
+        </div>
+    </a><a href='https://www.ottplay.com/news/get-a-free-disney-plus-hotstar-subscription-with-these-airtel-plans/92494ee20b215'>
+        <div className={styles.card} style={{ maxWidth: "90%", width: md ? "auto": "90vh"}}>
+            <h3>Get a free Disney Plus Hotstar subscription with these Airtel plans</h3>
+            <div style={{ display: "flex" }}>
+            <Image src="https://t1.gstatic.com/images?q=tbn:ANd9GcTq3Tzz04DhaJigj9Rk4DSeYKs_UgHVwWW5ZmG3r7XO8CZ61ZhdgMRgfzFVdqTko64auCbng9X-3Q" alt="Get a free Disney Plus Hotstar subscription with these Airtel plans" height={150} width={150} style={{marginLeft: "15px"}}/>
+            <p style={{marginLeft: "15px"}}>Airtel offers some great options if you&#39;re its subscriber who wants to watch your favourite shows or the latest movies on Disney Hotstar.</p></div>
+        </div>
+    </a><a href='https://www.prameyanews.com/good-news-for-airtel-users-get-free-subscriptions-to-disney-hotstar-amazon-prime/'>
+        <div className={styles.card} style={{ maxWidth: "90%", width: md ? "auto": "90vh"}}>
+            <h3>Good news for Airtel users! Get free subscriptions to Disney+ Hotstar ...</h3>
+            <div style={{ display: "flex" }}>
+            <Image src="https://t2.gstatic.com/images?q=tbn:ANd9GcTdILvJGPSg_lpB1HS3vWWMpltgb52Yz-Ye_alRlFLFyrL95TnOtUmENn2eKIE1viSsuKI8aiEW" alt="Good news for Airtel users! Get free subscriptions to Disney+ Hotstar ..." height={150} width={150} style={{marginLeft: "15px"}}/>
+            <p style={{marginLeft: "15px"}}>Airtel has several recharge plans that offer free Disney plus Hotstar subscription. Now the users can avail free OTT series by paying a little extra than the&nbsp;...</p></div>
+        </div>
+    </a>
+        </Template></>;
+}
---
+++ pages/trending/vinod-arya.p.js
@@ -0,0 +1,58 @@
+import {Template} from "../_template";
+import styles from "../../styles/Projects.module.css";
+import Head from "next/head";
+import { useMediaQuery } from './../useMediaQuery';
+import Image from "next/image";
+
+export default function Page() {
+    const md = useMediaQuery(960);
+    return <><Head>
+        <title>Vinod Arya</title></Head><Template>
+            <h1 style={{fontSize: "30"}}>Vinod Arya</h1>
+            <Image width={800} height={500} src="" alt="Vinod Arya"/>
+            <h3>Recent News</h3>
+            <a href='https://www.hindustantimes.com/india-news/breaking-news-live-updates-september-24-latest-101663977705033.html'>
+        <div className={styles.card} style={{ maxWidth: "90%", width: md ? "auto": "90vh"}}>
+            <h3>LIVE: U&#39;khand BJP leader expelled from party as son held in teen&#39;s ...</h3>
+            <div style={{ display: "flex" }}>
+            <Image src="https://t3.gstatic.com/images?q=tbn:ANd9GcQfdZJVF00S4k8blELByUECEtEh1sn4Ob-XUYdpvSxPrWg3iULEBcV0Z6XTSk4GeQ98JBwge1RB" alt="LIVE: U&#39;khand BJP leader expelled from party as son held in teen&#39;s ..." height={150} width={150} style={{marginLeft: "15px"}}/>
+            <p style={{marginLeft: "15px"}}>Breaking news LIVE updates September 24, 2022: Get latest news, breaking news, latest updates, live news, top headlines, breaking business news and top news&nbsp;...</p></div>
+        </div>
+    </a><a href='https://www.indiatoday.in/india/story/bjp-leader-vinod-arya-son-pulkit-arya-arrested-for-murder-of-teenager-in-uttarakhand-2004103-2022-09-23'>
+        <div className={styles.card} style={{ maxWidth: "90%", width: md ? "auto": "90vh"}}>
+            <h3>BJP leader&#39;s son arrested over murder of Uttarakhand girl who ...</h3>
+            <div style={{ display: "flex" }}>
+            <Image src="https://t0.gstatic.com/images?q=tbn:ANd9GcTZqe4h1bo_DcmY7diuIUX4V1EbMXH3gqU1vkwZU_E4cD61NdhvVYCWR48pu9BLqvvqI5V9Rc1Rew" alt="BJP leader&#39;s son arrested over murder of Uttarakhand girl who ..." height={150} width={150} style={{marginLeft: "15px"}}/>
+            <p style={{marginLeft: "15px"}}>Pulkit Arya, the son of a BJP leader, is the main accused in the murder case of the 19-year-old girl who went missing in Uttarakhand. He is the owner of the&nbsp;...</p></div>
+        </div>
+    </a><a href='https://www.tribuneindia.com/news/nation/bjp-expels-ankita-murder-accuseds-father-vinod-arya-and-brother-ankit-from-party-434911'>
+        <div className={styles.card} style={{ maxWidth: "90%", width: md ? "auto": "90vh"}}>
+            <h3>BJP expels Ankita murder accused&#39;s father Vinod Arya and brother ...</h3>
+            <div style={{ display: "flex" }}>
+            <Image src="https://t3.gstatic.com/images?q=tbn:ANd9GcQpmpLM7nVn1iKFPeQaBcO3moeMwU5Et_4vSwSYQel2r-_kpgSq1ZBjukHm-CGi2qySOL99_CkQ" alt="BJP expels Ankita murder accused&#39;s father Vinod Arya and brother ..." height={150} width={150} style={{marginLeft: "15px"}}/>
+            <p style={{marginLeft: "15px"}}>As public anger mounted over the murder of 19-year-old Ankita Bhandari, the BJP on Saturday expelled accused Pulkit Arya&#39;s father Vinod Arya and brother&nbsp;...</p></div>
+        </div>
+    </a><a href='https://www.outlookindia.com/national/uttarakhand-bjp-leader-s-son-and-two-others-arrested-for-killing-receptionist-of-his-resort-news-225453'>
+        <div className={styles.card} style={{ maxWidth: "90%", width: md ? "auto": "90vh"}}>
+            <h3>Uttarakhand Receptionist Killed: Illegal Resort Demolished On CM ...</h3>
+            <div style={{ display: "flex" }}>
+            <Image src="https://t0.gstatic.com/images?q=tbn:ANd9GcTM2SDaszrGbmc9YAUpmiPUK6D_PLKNmfJrfEO9_GRXXY5ZlUiL0QavDPAQq01FQWthW7JShUIW" alt="Uttarakhand Receptionist Killed: Illegal Resort Demolished On CM ..." height={150} width={150} style={{marginLeft: "15px"}}/>
+            <p style={{marginLeft: "15px"}}>Police early Saturday recovered the body of the receptionist of the resort from Cheela canal where the accused had dumped her. The resort, built illegally&nbsp;...</p></div>
+        </div>
+    </a><a href='https://indianexpress.com/article/india/uttarakhand-bjp-leader-son-arrested-killing-receptionist-8169132/'>
+        <div className={styles.card} style={{ maxWidth: "90%", width: md ? "auto": "90vh"}}>
+            <h3>Uttarakhand ex-minister&#39;s son held for killing 19-yr-old</h3>
+            <div style={{ display: "flex" }}>
+            <Image src="https://t3.gstatic.com/images?q=tbn:ANd9GcR89o3oPDuzW1a6c25zFWSIcymt4zLeVgC64Gaa2i3l-Bo4r73Q3Joo_ic9yhp1v502wUIt4rpB" alt="Uttarakhand ex-minister&#39;s son held for killing 19-yr-old" height={150} width={150} style={{marginLeft: "15px"}}/>
+            <p style={{marginLeft: "15px"}}>Pulkit Arya, the son of BJP leader Vinod Arya, has been named as the “main accused” in the case, the police said. Vinod Arya was a Minister of State in the&nbsp;...</p></div>
+        </div>
+    </a><a href='https://www.latestly.com/socially/india/news/ankita-bhandari-murder-case-bjp-expels-vinod-arya-and-ankit-arya-the-father-and-latest-tweet-by-ani-uputtarakhand-4243881.html'>
+        <div className={styles.card} style={{ maxWidth: "90%", width: md ? "auto": "90vh"}}>
+            <h3>Ankita Bhandari Murder Case: Pulkit Arya&#39;s Father Vinod Arya and ...</h3>
+            <div style={{ display: "flex" }}>
+            <Image src="https://t0.gstatic.com/images?q=tbn:ANd9GcQi8dkEpU6JiJxI-Tj5Ow-6Wp11_I4yCEnou2GwCTwlvmopRS-ZprEklcUmdgIyDPAAqk-KK5Hy" alt="Ankita Bhandari Murder Case: Pulkit Arya&#39;s Father Vinod Arya and ..." height={150} width={150} style={{marginLeft: "15px"}}/>
+            <p style={{marginLeft: "15px"}}>The Bhartiya Janata Party (BJP) on Saturday expelled party leaders Vinod Arya and Ankit Arya - the father and brother of main accused Pulkit Arya,&nbsp;...</p></div>
+        </div>
+    </a>
+        </Template></>;
+}
