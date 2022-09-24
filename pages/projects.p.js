7484533581508c5714a618f78d76856f9c563eae
Devesh Pal <newdev0@outlook.com>
2022-09-24 09:29:54 +0530

    Update

--- pages/projects.p.js
+++ pages/projects.p.js
@@ -2,6 +2,7 @@ import { Template } from "./_template"
 import styles from "../styles/Projects.module.css";
 import Head from "next/head";
 import Image from "next/image";
+import { useMediaQuery } from "./useMediaQuery";
 
 function gh(repo) {
     return `https://github.com/New-dev0/${repo}`;
@@ -9,7 +10,7 @@ function gh(repo) {
 
 function CreateCard({ title, description, image, href, style }) {
     return <a href={href || title.toLowerCase()}><div className={styles.card} style={style}>
-        <h2>{title}</h2> {!image && <p style={{ paddingLeft: "2rem" }}>{description}</p>}
+       <h2>{title}</h2> {!image && <p style={{ paddingLeft: "2rem" }}>{description}</p>}
         {image && <div style={{ display: "flex" }}>
             <Image src={image} width="200" height="100" alt={title} />
             <div>{description}</div>
@@ -19,16 +20,17 @@ function CreateCard({ title, description, image, href, style }) {
 }
 
 export default function Projects() {
+    const query = useMediaQuery(960);
     return <>
         <Head>
             <title>Projects</title>
         </Head>
         <Template show_content={false}>
             <h1 className={styles.heading}>Projects</h1>
-            <CreateCard title="TgTwitterStreamer" description={"Automate posting of Twitter tweets on Telegram."} image="https://raw.githubusercontent.com/New-dev0/TgTwitterStreamer/main/TgTwitterStreamer/assets/START.webp" />
-            <CreateCard title="Template-Generater" description="Create profile templates for Github, Telegram and more." href="https://template.newdev0.in" style={{minWidth: "20rem"}}/>
-            <CreateCard title="TgTwitterBot" description={"Control and Explore Twitter from Telegram."} image="https://raw.githubusercontent.com/New-dev0/TgTwitterStreamer/main/TgTwitterStreamer/assets/START.webp" />
-            <CreateCard title="Bing-Chatbot" description={"Bing AI Based Telegram Chat bot."} href={gh("Bing-ChatBot")}  style={{minWidth: "20rem"}}/>
+            <CreateCard title="TgTwitterStreamer" description={"Automate posting of Twitter tweets on Telegram."} image="https://raw.githubusercontent.com/New-dev0/TgTwitterStreamer/main/TgTwitterStreamer/assets/START.webp" style={{width: query ? "60vh": "90vh"}} />
+            <CreateCard title="Template-Generater" description="Create profile templates for Github, Telegram and more." href="https://template.newdev0.in" style={{minWidth: "20rem", width: query ? "60vh": "90vh"}}/>
+            <CreateCard title="TgTwitterBot" description={"Control and Explore Twitter from Telegram."} image="https://raw.githubusercontent.com/New-dev0/TgTwitterStreamer/main/TgTwitterStreamer/assets/START.webp" style={{width: query ? "60vh": "90vh"}}/>
+            <CreateCard title="Bing-Chatbot" description={"Bing AI Based Telegram Chat bot."} href={gh("Bing-ChatBot")}  style={{minWidth: "20rem", width: query ? "60vh": "90vh"}}/>
         </Template>
     </>
 }
