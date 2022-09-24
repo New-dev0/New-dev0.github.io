9c624d49b504bed1d8e9dde350f4873251dde155
Devesh Pal <newdev0@outlook.com>
2022-09-24 15:36:30 +0530

    Update

--- pages/_document.p.js
+++ pages/_document.p.js
@@ -2,6 +2,22 @@ import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import Script from "next/script";
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTv518AQlwNWO2RMgcLMQilFelf4xp3q8",
+  authDomain: "sitedev-75486.firebaseapp.com",
+  projectId: "sitedev-75486",
+  storageBucket: "sitedev-75486.appspot.com",
+  messagingSenderId: "330395980635",
+  appId: "1:330395980635:web:c0693ddd438c0f581d407e",
+  measurementId: "G-R8EX2K8DB0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
