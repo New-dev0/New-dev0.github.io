63de0c37dde4d183f984e88c19c15331b8b75945
Devesh Pal <newdev0@outlook.com>
2022-09-25 10:29:40 +0530

    Update

---
+++ pages/trending/index.p.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+
+export default function Page() {
+    return <>
+    <Head>
+        <meta name="keywords" content="Trending,news,page,content" />
+        <title>Trending</title>
+        <meta name="description" content="Get Notified of Content." />
+    </Head>
+    <h3>
+        Place of Trending News.</h3></>
+}
