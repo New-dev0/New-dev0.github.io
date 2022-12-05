a228f388fbc2cb0349e7d84ef775f9f59c60dfe7
Devesh Pal <newdev0@outlook.com>
2022-12-05 19:30:57 +0530

    update

--- pages/_app.p.js
+++ pages/_app.p.js
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
 import '../styles/globals.css';
-import { NextUIProvider } from '@nextui-org/react';
+import { analytics } from './firebase';
+import { logEvent } from 'firebase/analytics';
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    logEvent(analytics(), "page_view", {
+      page_location: window.location.href,
+      page_path: window.location.pathname,
+      page_title: document.title
+    })
+  }, []
+  );
+
   return <>
-      <Component {...pageProps} />
+    <Component {...pageProps} />
   </>
 }
 
--- pages/_document.p.js
+++ pages/_document.p.js
@@ -1,23 +1,5 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from "next/script";
-import { getAnalytics, isSupported } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDTv518AQlwNWO2RMgcLMQilFelf4xp3q8",
-  authDomain: "sitedev-75486.firebaseapp.com",
-  projectId: "sitedev-75486",
-  storageBucket: "sitedev-75486.appspot.com",
-  messagingSenderId: "330395980635",
-  appId: "1:330395980635:web:c0693ddd438c0f581d407e",
-  measurementId: "G-R8EX2K8DB0"
-};
-
-// Initialize Firebase
-app = initializeApp(firebaseConfig);
-const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
-
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
