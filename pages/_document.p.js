64b45abda53568ce4548cf5c0d77c8ffef250a51
Devesh Pal <newdev0@outlook.com>
2023-03-28 13:10:50 +0530

    test

--- pages/_document.p.js
+++ pages/_document.p.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -14,12 +15,15 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="theme-color" content="#003399" />
-          </Head>
+        </Head>
         <body>
           <Main />
           <NextScript />
-         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4464694750922288"
-     crossorigin="anonymous" crossOrigin="anonymous"></script>
+          <p>
+            <script src='https://inklinkor.com/tag.min.js' data-zone="5399934" async></script>
+          </p>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4464694750922288"
+            crossOrigin="anonymous"></script>
         </body>
       </Html>
     );
